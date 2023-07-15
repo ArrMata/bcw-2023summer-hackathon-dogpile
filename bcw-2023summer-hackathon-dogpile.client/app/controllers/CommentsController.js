e93825c5c2d9eaa54fc0e0605d18ac2dedfd42f0
Bailey <bailey91716@gmail.com>
2023-07-14 20:36:52 -0600

    create comments, form, update post

--- bcw-2023summer-hackathon-dogpile.client/app/controllers/CommentsController.js
+++ bcw-2023summer-hackathon-dogpile.client/app/controllers/CommentsController.js
@@ -1,5 +1,6 @@
 import { AppState } from "../AppState.js";
 import { commentsService } from "../services/CommentsService.js";
+import { getFormData } from "../utils/FormHandler.js";
 import { Pop } from "../utils/Pop.js";
 import { setHTML } from "../utils/Writer.js";
 
@@ -29,4 +30,17 @@ export class CommentsController {
     }
   }
 
+  async createComment(event) {
+    try {
+      event.preventDefault()
+      let form = event.target
+      let formData = getFormData(form)
+      // console.log(formData);
+      await commentsService.createComment(formData)
+      form.reset()
+    } catch (error) {
+      console.error(error)
+      Pop.error(error.message)
+    }
+  }
 }
