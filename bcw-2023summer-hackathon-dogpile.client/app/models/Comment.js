2b97ba06faff0d1ef7fffc9c72eee89becab78de
Arrington Mata <mataarrington@gmail.com>
2023-07-16 15:08:21 -0600

    updated comment styling

--- bcw-2023summer-hackathon-dogpile.client/app/models/Comment.js
+++ bcw-2023summer-hackathon-dogpile.client/app/models/Comment.js
@@ -1,15 +1,23 @@
 export class Comment {
   constructor(data) {
-    // console.log('comment data', data);
+    console.log('comment data', data);
     this.content = data.content
     this.id = data.id
     this.commentorId = data.commentorId
+    this.commentorName = data.commentor.name
+    this.commentorPicture = data.commentor.picture
     this.postId = data.postId
   }
 
   get CommentTemplate() {
     return `
-  <p class="fs-4">${this.content}</p>
+    <div class="comment">
+      <div class="comment-user">
+        <img src="${this.commentorPicture}" alt="${this.commentorName}" />
+        <h5>${this.commentorName}</h5>
+      </div>
+      <p class="fs-4">${this.content}</p>
+    </div>
 `
   }
 }
